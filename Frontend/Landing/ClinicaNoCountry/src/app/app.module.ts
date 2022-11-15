be0f86fc43fa21dd23b8a273b03d2cc30ddbba31
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2022-11-15 16:12:01 -0300

    Estructura base

--- Frontend/Landing/ClinicaNoCountry/src/app/app.module.ts
+++ Frontend/Landing/ClinicaNoCountry/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
