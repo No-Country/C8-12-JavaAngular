fca894d81ac77de2fb61c7c42c81a4e527c0845c
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2022-11-28 16:31:38 -0300

    nuevo

--- Frontend/Landing/ClinicaNoCountry/src/app/pages/login/login.component.ts
+++ Frontend/Landing/ClinicaNoCountry/src/app/pages/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
+ 
+  number?: number;
+  password?: string;
 
   constructor() { }
 
-  ngOnInit(): void {
+  login() {
+    console.log(this.number);
+    console.log(this.password);
   }
+  
 
 }
