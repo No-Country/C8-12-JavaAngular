b53d8d9c6906a6f03bc342758956df79b76633e7
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2022-12-05 11:51:34 -0300

    home admin 2.0

--- Frontend/Landing/ClinicaNoCountry/src/app/pages/home-admin/home-admin.component.ts
+++ Frontend/Landing/ClinicaNoCountry/src/app/pages/home-admin/home-admin.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+export interface turnosPaciente {
+  fecha: string ;
+  hora: string;
+  direccion: string;
+  especialidad: string;
+  profesional: string;
+}
+
+const ELEMENT_DATA: turnosPaciente[] = [
+  {fecha: 'Lunes 9/12/2022', hora: '11:30 a.m', direccion:'San Martín 360, CABA', especialidad: 'Oftalmología', profesional: "Dra. Gonzalez Claudia"},
+  {fecha: 'Jueves 11/11/22', hora: '10:00 a.m', direccion: 'San Martín 360, CABA', especialidad: 'Cardiología', profesional: "Dr. Perez Esteban"},
+  {fecha: 'Martes 22/11/22', hora: '12:15 p.m', direccion: 'San Martín 360, CABA', especialidad: 'Dermatología', profesional: "Dra. Fernandez Liliana"},
+
+];
+
+/**
+ * @title Binding event handlers and properties to the table rows.
+ */
+
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
   styleUrls: ['./home-admin.component.css']
 })
-export class HomeAdminComponent implements OnInit {
+export class HomeAdminComponent  implements OnInit {
+
+  displayedColumns: string[] = ['fecha', 'hora', 'direccion', 'especialidad', 'profesional'];
+  datasource = ELEMENT_DATA;
 
   constructor() { }
 
