46e6d2cb541d420467086987b0998753651a5ba7
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2022-12-05 14:24:01 -0300

    Home Admin Casi final

--- Frontend/Landing/ClinicaNoCountry/src/app/pages/home-admin/home-admin.component.ts
+++ Frontend/Landing/ClinicaNoCountry/src/app/pages/home-admin/home-admin.component.ts
@@ -1,38 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 export interface turnosPaciente {
-  fecha: string ;
+  paciente: string;
+  fecha: string;
   hora: string;
-  direccion: string;
-  especialidad: string;
-  profesional: string;
+  identidad: string;
+  estado: string;
+  
 }
 
 const ELEMENT_DATA: turnosPaciente[] = [
-  {fecha: 'Lunes 9/12/2022', hora: '11:30 a.m', direccion:'San Martín 360, CABA', especialidad: 'Oftalmología', profesional: "Dra. Gonzalez Claudia"},
-  {fecha: 'Jueves 11/11/22', hora: '10:00 a.m', direccion: 'San Martín 360, CABA', especialidad: 'Cardiología', profesional: "Dr. Perez Esteban"},
-  {fecha: 'Martes 22/11/22', hora: '12:15 p.m', direccion: 'San Martín 360, CABA', especialidad: 'Dermatología', profesional: "Dra. Fernandez Liliana"},
-
+  {
+    fecha: 'Lunes 9/12/2022',
+    hora: '11:30 a.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Oftalmología',
+    paciente: 'Farias, Pedro',
+  },
+  {
+    fecha: 'Jueves 11/11/22',
+    hora: '10:00 a.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Cardiología',
+    paciente: 'Salutri, Juana',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Fuentes, Marcelo',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Garcia, Lautaro',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Martinez, Julieta',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Hernández, Laura',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Perez, Julian',
+  },
+  {
+    fecha: 'Martes 22/11/22',
+    hora: '12:15 p.m',
+    identidad: 'San Martín 360, CABA',
+    estado: 'Dermatología',
+    paciente: 'Munro, Esteban',
+  },
 ];
 
 /**
  * @title Binding event handlers and properties to the table rows.
  */
 
-
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
-  styleUrls: ['./home-admin.component.css']
+  styleUrls: ['./home-admin.component.css'],
 })
-export class HomeAdminComponent  implements OnInit {
-
-  displayedColumns: string[] = ['fecha', 'hora', 'direccion', 'especialidad', 'profesional'];
+export class HomeAdminComponent implements OnInit {
+  displayedColumns: string[] = [
+    'paciente',
+    'fecha',
+    'hora',
+    'identidad',
+    'estado',
+  ];
   dataSource = ELEMENT_DATA;
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
